04e5181eb0b82923ff9803c7d8dd0ffb8874f4fb
groudstation <groudstation@C305-BT100026.fieb.org.br>
2025-07-17 13:08:05 -0300

    feat: add fastify-multipart

--- src/server.ts
+++ src/server.ts
@@ -6,16 +6,19 @@ import {
 } from "fastify-type-provider-zod";
 import { env } from "./env.ts";
 import fastifyCors from "@fastify/cors";
+import { fastifyMultipart } from "@fastify/multipart";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 import { createRoomRoute } from "./http/routes/create-rooms.ts";
 import { getRoomQuestions } from "./http/routes/get-room-questions.ts";
 import { createQuestionRoute } from "./http/routes/create-question.ts";
-
+import { uploadAudioRoute } from "./http/routes/upload-audio.ts";
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: "http://localhost:5173",
 });
+
+app.register(fastifyMultipart);
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -28,6 +31,7 @@ app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomQuestions);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT ? Number(env.PORT) : 3333 }).then(() => {
   console.log(`http server running! as ${env.PORT} `);
