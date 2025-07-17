1b1640e4d082febdd69605455cadbdf927de8abd
groudstation <groudstation@C305-BT100026.fieb.org.br>
2025-07-17 13:11:45 -0300

    feat: add audio-chunks on schema

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,7 +1,8 @@
 import { rooms } from "./rooms.ts";
 import { questions } from "./questions.ts";
-
+import { audioChunks } from "./audio-chunks.ts";
 export const schema = {
   rooms,
   questions,
+  audioChunks,
 };
